6bf6b7b6139653d80cfb164041655d944b3a95a5
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-04-08 15:46:08 -0400

    MVP complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,35 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement === 20){
+    return { ...item };
+  }else{
+    item.enhancement += 1
+    return { ...item };
+  }
 }
 
 function fail(item) {
+  if(item.enhancement < 15){
+    item.durability -= 5;
+  }else if(item.enhancement > 15){
+    item.durability -= 10;
+
+    if(item.enhancement > 16){
+      item.enhancement -= 1;
+    }
+  }
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  if(item.durability === 100){
+    return {...item};
+  }else{
+    item.durability = 100;
+    return {...item};
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,51 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+let exampleFullItem = {
+    name: "item name",
+    durability: 100,
+    enhancement: 20
+}
+let exampleDurabilityReducedItem = {
+    name: "item name",
+    durability: 50,
+    enhancement: 20
+}
+let exampleEnhReducedItem = {
+    name: "item name",
+    durability: 100,
+    enhancement: 14
+}
+let enh16 = {
+    name: "item name",
+    durability: 100,
+    enhancement: 16
+}
+
+let less15 = {
+    name: "item name",
+    durability: 100,
+    enhancement: 14
+}
+
+describe('enhancer.js', () => {
+    describe('enhancer', () => {
+        it('should return a repaired object', () => {
+            expect(enhancer.repair(exampleFullItem)).toEqual({...exampleFullItem});
+            expect(enhancer.repair(exampleDurabilityReducedItem)).toEqual({...exampleDurabilityReducedItem, durability: 100});
+        }) 
+
+        it('should enhance the object according to the set rules...', () => {
+            expect(enhancer.succeed(exampleFullItem)).toEqual(exampleFullItem);
+            expect(enhancer.succeed(exampleEnhReducedItem)).toEqual({...exampleEnhReducedItem, enhancement: 15 });
+        }) 
+        it('should fail to enhance the object according to the set rules...', () => {
+                //greater than 16
+            expect(enhancer.fail(exampleFullItem)).toEqual({...exampleFullItem, durability: 90, enhancement: 19 });
+                //equals 16
+                expect(enhancer.fail(enh16)).toEqual({...enh16, durability: 90})
+                //less than 15
+                expect(enhancer.fail(less15)).toEqual({...less15, durability: 95})
+        }) 
+    })
+})
